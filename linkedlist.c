e32e08293191e85447a28b2ff54a5be90fe7c5c9
alexaorrico <alexaorrico@gmail.com>
2017-08-14 21:55:14 +0000

    linked list functions

---
+++ linkedlist.c
@@ -0,0 +1,58 @@
+#include "monty.h"
+
+/**
+ * add_node_start - add a new node at the beginning of the linked list
+ * @stack: double pointer to the beginning of the linked list
+ * @n: value to add to the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+stack_t *add_node_start(stack_t **stack, const int n)
+{
+	stack_t *new;
+
+	if (stack == NULL)
+		return (NULL);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *stack;
+	*stack = new;
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
+}
+
+/**
+ * add_node_end - adds a node to the end of a linked list
+ * @stack: double pointer to the beginning of the linked list
+ * @n: value to add to new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+stack_t *add_node_end(stack_t **head, const int n)
+{
+	stack_t *new, *tmp;
+
+	if (stack == NULL)
+		return (NULL);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*stack == NULL)
+	{
+		new->prev = NULL;
+		*stack = new;
+		return (new);
+	}
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
+}
