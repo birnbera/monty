64d3f09c2f7c6701326a108ce42ecc9e4d6ebdce
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-08-14 23:15:11 +0000

    add nop operation

---
+++ m_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * m_nop - no operation performed
+ * @stack: double pointer to head of stack
+ * @line_number: line number of current operation
+ *
+ * Return: void
+ */
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
