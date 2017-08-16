cee24cda0eb72797d7250b44e7de926c261f6b18
alexaorrico <alexaorrico@gmail.com>
2017-08-16 17:10:45 +0000

    fix for negative numbers and add tests

--- push.c
+++ push.c
@@ -12,8 +12,12 @@ static int check_for_digit(char *arg)
 	int i;
 
 	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
 		if (isdigit(arg[i]) == 0)
 			return (1);
+	}
 	return (0);
 }
 
